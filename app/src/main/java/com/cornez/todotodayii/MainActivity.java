56e8f42e985a3870c27b3089de45cf9e8f888194
PratikshaMore <morepratiksha9@gmail.com>
2017-12-19 16:56:28 -0500

    Fixes focus bug in add/edit history page

--- app/src/main/java/com/cornez/todotodayii/MainActivity.java
+++ app/src/main/java/com/cornez/todotodayii/MainActivity.java
@@ -59,9 +59,9 @@ public class MainActivity extends Activity {
         login_name = username.getText().toString();
         login_password = password.getText().toString();
 
-//        TODO UNCOMMENT THIS BEFORE FINALS
-//      if((login_name.matches("Keerthana") && login_password.matches("abcd"))|| (login_name.matches("Pratiksha")&& login_password.matches("1234")))
-        if(true)
+    // if(true)
+      if((login_name.matches("Keerthana") && login_password.matches("abcd"))|| (login_name.matches("Pratiksha")&& login_password.matches("1234")))
+
         {
             Intent intent1 = new Intent(getBaseContext(), PetListActivity.class);
             startActivity(intent1);
